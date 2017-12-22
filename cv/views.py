4d873777a2d14fdd27507843873265a10ebb825e
tessernest <tessernest@hotmail.com>
2017-12-22 10:47:45 +0100

    last details

--- cv/views.py
+++ cv/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, get_object_or_404
 from django.utils import timezone
 from .models import Post
+from .forms import PostForm
 
 
 def post_list(request):
@@ -12,3 +13,7 @@ def post_list(request):
 def post_detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
     return render(request, 'cv/post_detail.html', {'post': post})
+
+def post_new(request):
+    form = PostForm()
+    return render(request, 'cv/post_edit.html', {'form': form})
